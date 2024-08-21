90141dc896594730bd1a3d2248ef0a58af19cd0a
gabreudev <biel0alves0@gmail.com>
2024-08-21 10:15:50 -0300

    trash: cleaning not used importations

--- src/main/java/com/gabreudev/marketmobile_api/controllers/ProductController.java
+++ src/main/java/com/gabreudev/marketmobile_api/controllers/ProductController.java
@@ -1,13 +1,11 @@
 package com.gabreudev.marketmobile_api.controllers;
 
 import com.gabreudev.marketmobile_api.entities.Product;
-import com.gabreudev.marketmobile_api.exceptions.ProductNotFoundException;
 import com.gabreudev.marketmobile_api.servicies.ProductService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
