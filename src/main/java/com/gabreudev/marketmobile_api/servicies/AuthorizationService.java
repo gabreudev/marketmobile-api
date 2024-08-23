0315acf0bfadc6c3d472085a14e9c2980d235ac2
gabreudev <biel0alves0@gmail.com>
2024-08-23 17:37:25 -0300

    feat: rename service user to authorization

--- src/main/java/com/gabreudev/marketmobile_api/servicies/AuthorizationService.java
+++ src/main/java/com/gabreudev/marketmobile_api/servicies/AuthorizationService.java
@@ -1,5 +1,4 @@
 package com.gabreudev.marketmobile_api.servicies;
-
 import com.gabreudev.marketmobile_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,13 +7,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements UserDetailsService {
+public class AuthorizationService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+
+    public UserDetails loadUSerByEmail(String email){
         return repository.findByEmail(email)
                 .orElseThrow( ()-> new UsernameNotFoundException(String.format("usuario com e emial %s não foi encontrado", email)));
     }
