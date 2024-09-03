42bf922621ad9ccfdc477318f8bfa892c03f81c8
gabreudev <biel0alves0@gmail.com>
2024-09-03 15:05:37 -0300

    feat: more methods and configs

--- src/main/java/com/gabreudev/marketmobile_api/servicies/SubscriptionService.java
+++ src/main/java/com/gabreudev/marketmobile_api/servicies/SubscriptionService.java
@@ -3,8 +3,11 @@ package com.gabreudev.marketmobile_api.servicies;
 import com.gabreudev.marketmobile_api.entities.user.User;
 import com.stripe.Stripe;
 import com.stripe.model.Customer;
+import com.stripe.model.Subscription;
+import com.stripe.model.SubscriptionCollection;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.SubscriptionListParams;
 import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -25,8 +28,8 @@ public class SubscriptionService {
                 .setCustomer(user.getCustomerId())
                 .setMode(SessionCreateParams.Mode.SUBSCRIPTION)
                 .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-                .setSuccessUrl("https://your-success-url.com/success")
-                .setCancelUrl("https://your-cancel-url.com/cancel")
+                .setSuccessUrl("https://www.youtube.com/watch?v=uKxyLmbOc0Q")
+                .setCancelUrl("https://www.youtube.com/watch?v=wEWF2xh5E8s")
                 .addLineItem(
                         SessionCreateParams.LineItem.builder()
                                 .setPrice(priceId)
@@ -44,4 +47,27 @@ public class SubscriptionService {
 
         return Customer.create(params);
     }
+    private Customer retrieveCustomer(String customerId) throws Exception {
+        return Customer.retrieve(customerId);
+    }
+    private SubscriptionCollection retrieveCustomerSubscriptions(String customerId) throws Exception {
+        SubscriptionListParams params = SubscriptionListParams.builder()
+                .setCustomer(customerId)
+                .build();
+
+        return Subscription.list(params); // Retorna todas as assinaturas do cliente
+    }
+
+    private boolean isSubscriptionActive(String customerId) throws Exception {
+        SubscriptionCollection subscriptions = retrieveCustomerSubscriptions(customerId);
+
+        for (Subscription subscription : subscriptions.getData()) {
+            if ("active".equals(subscription.getStatus())) {
+                return true; // Assinatura ativa encontrada
+            }
+        }
+
+        return false; // Nenhuma assinatura ativa encontrada
+    }
+
 }
